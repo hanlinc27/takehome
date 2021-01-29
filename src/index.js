1e966dc2a2c04295dd3b02116eea7a7292e0a285
Hanlin Cheng <h62cheng@uwaterloo.ca>
2021-01-29 01:38:03 -0500

    small fix

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-const proxy = require('http-proxy-middleware')
 
-module.exports = function(app) {
-    app.use(proxy(['/api' ], { target: 'http://localhost:5000' }));
-} 
 ReactDOM.render(
   <React.StrictMode>
     <App />
